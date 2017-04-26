e452d3895110f4c3bb26b07dc3ec1a090e8889dd
Quisy <lamprecht.mariusz@gmail.com>
2017-04-26 20:22:43 +0200

    Added ViewModel to MainPage, added First and Last Name TextBoxes with data binding

--- UWP.Sample.App.Csharp/App/MainPage.xaml.cs
+++ UWP.Sample.App.Csharp/App/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using App.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,6 +27,7 @@ namespace App
         public MainPage()
         {
             this.InitializeComponent();
+            this.DataContext = new MainPageViewModel();
         }
 
         private async void Button_HelloWorld_Click(object sender, RoutedEventArgs e)
---
+++ UWP.Sample.App.Csharp/App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Models;
+using App.ViewModels.Base;
+
+namespace App.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                RaisePropertyChanged(nameof(UserFullName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                RaisePropertyChanged(nameof(UserFullName));
+            }
+        }
+
+        public string UserFullName => $"{FirstName} {LastName}";
+
+        public MainPageViewModel()
+        {
+
+            FirstName = "User FirstName";
+            LastName = "User LastName";
+        }
+    }
+}
